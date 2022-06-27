c448a25e98a8a787961981333e10efd380953cd3
Barigamb738 <54997181+Barigamb738@users.noreply.github.com>
2022-06-27 16:22:25 +0200

    Add files via upload

---
+++ bulkAnimator.py
@@ -0,0 +1,21 @@
+import animator
+import split
+import cv2
+import numpy as np
+import os
+
+projectName = "rickroll"
+fullmp4Path = "2xrickroll.mp4"
+
+if not os.path.isdir(projectName):
+    os.mkdir(projectName)
+
+#split.split_by_seconds(fullmp4Path, 2.4, projectName)
+
+
+videos = [f for f in os.listdir(projectName)]
+
+print(videos)
+
+for video in videos:
+    animator.wysAnimate(60, video, video)
